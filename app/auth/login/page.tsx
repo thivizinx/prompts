73f22909db2c562163dccdec668251e68592b0e7
Thiago Henrique Lacerda Ferreira <thiagohenri12332@gmail.com>
2026-02-20 01:12:37 -0300

    nao pode mais criar conta

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -89,12 +89,7 @@ export default function Page() {
                 </div>
                 <div className="mt-4 text-center text-sm">
                   Don&apos;t have an account?{' '}
-                  <Link
-                    href="/auth/sign-up"
-                    className="underline underline-offset-4"
-                  >
-                    Sign up
-                  </Link>
+              
                 </div>
               </form>
             </CardContent>
